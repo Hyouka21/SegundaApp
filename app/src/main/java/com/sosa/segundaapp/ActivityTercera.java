36aa7d1b97a88d6f8b6c56ee8a46e23609cd6459
Gaston <70915311+Hyouka21@users.noreply.github.com>
2021-08-11 18:53:20 -0300

    segunda comit

--- app/src/main/java/com/sosa/segundaapp/ActivityTercera.java
+++ app/src/main/java/com/sosa/segundaapp/ActivityTercera.java
@@ -2,13 +2,41 @@ package com.sosa.segundaapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
+import android.content.ContentResolver;
+import android.database.Cursor;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.CallLog;
+import android.util.Log;
+import android.widget.TextView;
 
 public class ActivityTercera extends AppCompatActivity {
-
+    private TextView TVfinal;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_tercera);
+        acceder();
+    }
+    @SuppressLint("Range")
+    private void acceder(){
+        TVfinal = findViewById(R.id.TVFinal);
+        Uri llamadas = Uri.parse("content://call_log/calls");
+        ContentResolver cr = getContentResolver();
+        Cursor c = cr.query(llamadas,null,null,null,null);
+        String nro=null;
+        String tiempo=null;
+        StringBuilder sb =new StringBuilder();
+        if(c != null && c.getCount()>0){
+            while(c.moveToNext()){
+                nro=c.getString(c.getColumnIndex(CallLog.Calls.NUMBER));
+                tiempo=c.getString(c.getColumnIndex(CallLog.Calls.DURATION));
+                sb.append("numero:"+nro+" duracion:"+tiempo+" \n");
+            }
+            TVfinal.setText(sb.toString());
+        }else{
+            TVfinal.setText("sin datos");
+        }
     }
 }
--- app/src/main/java/com/sosa/segundaapp/MainActivity.java
+++ app/src/main/java/com/sosa/segundaapp/MainActivity.java
@@ -2,7 +2,10 @@ package com.sosa.segundaapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.Manifest;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.os.Build;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -17,6 +20,11 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         inicializarVista();
+        if(Build.VERSION.SDK_INT>= Build.VERSION_CODES.M
+                && checkSelfPermission(Manifest.permission.READ_CALL_LOG)
+                != PackageManager.PERMISSION_GRANTED){
+            requestPermissions(new String[]{Manifest.permission.READ_CALL_LOG},1000);
+        }
     }
 
     private void inicializarVista() {
